42a63199cae1ce6a3319b0bdcccfa30a301cb4b0
Chayada-yam <yam.mx2005@gmail.com>
2026-02-14 20:58:04 +0700

    0.3

--- frontend/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center p-4 bg-gray-200 mt-10">
-      <p>© 2026 Luxury Spa. All rights reserved.</p>
-    </footer>
-  )
-}

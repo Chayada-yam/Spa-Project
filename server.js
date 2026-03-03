b2086519643465d9bb4413e7a4d41501ccd8e38b
Chayada-yam <yam.mx2005@gmail.com>
2026-03-03 13:21:22 +0700

    feat: add create customer API

--- server.js
+++ server.js
@@ -21,7 +21,24 @@ db.serialize(() => {
 
 // กำหนดให้ใช้โฟลเดอร์ public
 app.use(express.static(path.join(__dirname, 'public')));
+// ===== API SECTION =====
+app.use(express.json());
 
+app.post('/customers', (req, res) => {
+    const { name, phone } = req.body;
+
+    db.run(
+        `INSERT INTO customers (name, phone) VALUES (?, ?)`,
+        [name, phone],
+        function (err) {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.json({ id: this.lastID });
+        }
+    );
+});
+// ========================
 // เปิด server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);

19912e38dd4060a3ca7f6b1d1f480ac465fdbab5
Chayada-yam <yam.mx2005@gmail.com>
2026-03-03 13:16:13 +0700

    feat: create customers table

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const PORT = 3000;
+
+// ===== Database Setup =====
+const db = new sqlite3.Database('./spa.db');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            phone TEXT NOT NULL
+        )
+    `);
+});
+// ===========================
 
 // กำหนดให้ใช้โฟลเดอร์ public
 app.use(express.static(path.join(__dirname, 'public')));

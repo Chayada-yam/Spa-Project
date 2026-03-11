573435258008a08f2e889e43a46c51a6724751dc
Chayada-yam <yam.mx2005@gmail.com>
2026-03-11 22:04:22 +0700

    add password

---
+++ public/script.js
@@ -0,0 +1,45 @@
+function login(){
+
+const username=document.getElementById("username").value
+const password=document.getElementById("password").value
+
+const storedUser=JSON.parse(localStorage.getItem("user"))
+
+if(!storedUser){
+
+alert("ยังไม่มีผู้ใช้ กรุณาสมัครก่อน")
+return
+
+}
+
+if(username===storedUser.username && password===storedUser.password){
+
+alert("เข้าสู่ระบบสำเร็จ")
+
+window.location="services.html"
+
+}else{
+
+alert("เข้าสู่ระบบไม่สำเร็จ")
+
+}
+
+}
+
+
+function goAdmin(){
+
+const username=document.getElementById("username").value
+const password=document.getElementById("password").value
+
+if(username==="admin" && password==="1234"){
+
+window.location="admin.html"
+
+}else{
+
+alert("แอดมินไม่ถูกต้อง")
+
+}
+
+}

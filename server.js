002a7efe6c152bc6936ddb2cb4454fdd40c6956e
Thanaphon <hometail.com@gmail.com>
2026-03-11 21:50:38 +0700

    fix mysql connection

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express();
 
 // ================= DATABASE =================
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "spa_db",
+  port:3306
 });
 
 db.connect(err => {
